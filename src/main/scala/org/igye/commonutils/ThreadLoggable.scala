7cb970dac5ad9d8eae5410d6f0a82cb42dca86e5
Igorocky <Igorocky@users.noreply.github.com>
2015-11-18 14:54:18 +0100

    Add ThreadLoggable

---
+++ src/main/scala/org/igye/commonutils/ThreadLoggable.scala
@@ -0,0 +1,12 @@
+package org.igye.commonutils
+
+import java.lang.Thread.UncaughtExceptionHandler
+import java.util.Date
+
+import org.apache.logging.log4j.Logger
+
+class ThreadLoggable(implicit log: Logger) extends Thread(log.getName + "_%1$tF_%1$tT".format(new Date)) {
+    setUncaughtExceptionHandler(new UncaughtExceptionHandler {
+        override def uncaughtException(t: Thread, e: Throwable): Unit = log.error(e.getMessage, e)
+    })
+}
