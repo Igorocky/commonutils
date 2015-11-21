05907ea2b9af374b0a9a9b967d505b161c10b061
Igorocky <Igorocky@users.noreply.github.com>
2015-11-21 22:10:49 +0100

    Add marshaling to and unmarshaling from a file in JaxbSupport.scala

--- src/main/scala/org/igye/commonutils/JaxbSupport.scala
+++ src/main/scala/org/igye/commonutils/JaxbSupport.scala
@@ -1,24 +1,39 @@
 package org.igye.commonutils
 
-import java.io.{StringReader, StringWriter}
+import java.io.{File, StringReader, StringWriter}
 import javax.xml.bind.{Marshaller, JAXBContext}
 
 import scala.reflect.ClassTag
 
 object JaxbSupport {
     def marshal[T: ClassTag](anyObj: T): String = {
-        val jaxbContext = JAXBContext.newInstance(implicitly[ClassTag[T]].runtimeClass)
-        val jaxbMarshaller = jaxbContext.createMarshaller()
-        jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true)
         val res = new StringWriter()
-        jaxbMarshaller.marshal(anyObj, res)
+        createMarshaller(anyObj).marshal(anyObj, res)
         res.toString
     }
 
-    def unmarshal[T: ClassTag](str: String): T = {
+    def marshal[T: ClassTag](anyObj: T, file: File): Unit = {
+        createMarshaller(anyObj).marshal(anyObj, file)
+    }
+
+    private def createMarshaller[T: ClassTag](anyObj: T): Marshaller = {
         val jaxbContext = JAXBContext.newInstance(implicitly[ClassTag[T]].runtimeClass)
-        val jaxbUnmarshaller = jaxbContext.createUnmarshaller()
+        val jaxbMarshaller = jaxbContext.createMarshaller()
+        jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true)
+        jaxbMarshaller
+    }
+
+    def unmarshal[T: ClassTag](str: String): T = {
         val source = new StringReader(str)
-        jaxbUnmarshaller.unmarshal(source).asInstanceOf[T]
+        createUnmarshaller[T].unmarshal(source).asInstanceOf[T]
+    }
+
+    def unmarshal[T: ClassTag](file: File): T = {
+        createUnmarshaller[T].unmarshal(file).asInstanceOf[T]
+    }
+
+    private def createUnmarshaller[T: ClassTag] = {
+        val jaxbContext = JAXBContext.newInstance(implicitly[ClassTag[T]].runtimeClass)
+        jaxbContext.createUnmarshaller()
     }
 }
